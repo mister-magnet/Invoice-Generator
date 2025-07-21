b3b9abb4ea8a03cc067defda222936dd96f85c48
Mohitesh Kumar <mohiteshkumar18@gmail.com>
2025-07-21 18:02:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using InvoiceGenerator.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization(); 
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Invoice}/{action=Index}/{id?}");
+
+app.Run();
